198ec577a8219904387f07bbd3bb3d96f825ad1b
heartofgold <alessandro.biagiotti@studenti.unimi.it>
2024-08-25 10:17:03 +0200

    Added some corrections to the code for deletion

--- lists/list.c
+++ lists/list.c
@@ -17,13 +17,13 @@ list *emptylistconstructor() {
 void listdeconstructor(list *list) {
 	node *i = list->head;
 
-	while(i->next != NULL) {
+	while(i != NULL) {
 		node *j = i->next;
 		free(i);
 		i = j;
 	}
 
-free(i);
+	free(i);
 	free(list);
 	return;
 }
@@ -34,11 +34,13 @@ void push(list *list, data_type value) {
 
 	if(list->head == NULL) {
 		list->head = newnode;
+		list->length = 1;
 		return;
 	}
 
 	newnode->next = list->head;
 	list->head = newnode;
+	list->length = list->length + 1;
 }
 
 
@@ -46,23 +48,31 @@ node *pop(list *list) {
 	node *i = list->head;
 
 	list->head = list->head->next;
+	list->length = list->length - 1;
 
 	return i;
 }
 
 
 node *delete(list *list, data_type value) {
-	node *curr = list->head->next;
-	node *prev = list->head;
+	node *curr = list->head;
+	node *prev = NULL;
 
-	if(prev == NULL) {
+	if(curr == NULL) {
 		printf("The list is empty\n");
 		return NULL;
+	}
+
+	if(curr->value == value) {
+		list->head = curr->next;
+		list->length = list->length - 1;
+		return curr;
 	}
 
 	while(curr != NULL) {
 		if(curr->value == value) {
 			prev->next = curr->next;
+			list->length = list->length - 1;
 			return curr;
 		}
 		prev = curr;
