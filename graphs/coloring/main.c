d23d0a1507d3e6244b667958f3264deeb5549818
S3gmentati0nFault <alessandro.biagiotti@studenti.unimi.it>
2024-08-26 12:56:06 +0200

    Changes to the main for the coloring part'

--- graphs/coloring/main.c
+++ graphs/coloring/main.c
@@ -1,20 +1,36 @@
-#include "coloring.h"
 #include <stdio.h>
+
+#include "coloring.h"
+#include "../utilities/utilities.h"
+#include "../MIS/mis.h"
 
 int main() {
 	graph *G;
-	uint size = 4;
+	uint size = 1000;
 
 	// Construction of a random graph
-	G = randomgraphconstructor(size);
-	graphprinter(G);
+	G = randomsymmetricgraphconstructor(size);
+	if(size < 20) {
+		graphprinter(G);
+	}
+	graphwriter(G);
 
 	// Test naive coloring technique
 	int *colors = naivecoloring(G);
-	printf("vertex -> color\n");
-	for(uint i = 0; i < size; ++i) {
-		printf("%d -> %d\n", i, colors[i]);
+	if(size < 20) {
+		printf("vertex -> color\n");
+		for(uint i = 0; i < size; ++i) {
+			printf("%d -> %d\n", i, colors[i]);
+		}
 	}
+	coloringwriter(colors, size);
+	
+	// Computing the mis
+	//list *I = mis(G);
+	
+
+	// Testing the python checker
+	
 
 	graphdeconstructor(G);
 
