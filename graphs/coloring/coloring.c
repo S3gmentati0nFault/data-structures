8df19af365133795e3081bddc16d190785baa780
S3gmentati0nFault <alessandro.biagiotti@studenti.unimi.it>
2024-08-26 12:55:43 +0200

    Corrected the naivecoloring technique, added some more compilation clauses to the Makefile

--- graphs/coloring/coloring.c
+++ graphs/coloring/coloring.c
@@ -1,16 +1,18 @@
+#include <cstddef>
 #include <stdlib.h>
+#include <stdio.h>
 
-#include "../../priority-queues/priority.h"
+#include "../../lists/simple/list.h"
+#include "../MIS/mis.h"
 #include "coloring.h"
 
 
 int *naivecoloring(graph *G) {
-	// Construction of the pqueue
-	data_type *vector = (data_type *) malloc(G->size * sizeof(data_type));
+	// List construction
+	list *V = emptylistconstructor();
 	for(uint i = 0; i < G->size; ++i) {
-		vector[i] = i;
+		push(V, i);
 	}
-	pqueue *queue = vectorconstructor(vector, G->size);
 
 	// Resulting color array
 	int *colors = (int *) malloc(G->size * sizeof(int));
@@ -19,20 +21,76 @@ int *naivecoloring(graph *G) {
 	}
 
 	// Until the pile of vertices to be colored has not been handled:
-	while(!isempty(queue)) {
+	while(!isempty(V)) {
 		// Pick one vertex
-		uint vertex = dequeue(queue);
+		node *v = pop(V);
+		uint vertex = v->value;
 
 		// Pick the color in the neighbourhood
-		uint color = colorpicker(vertex, G, colors);
+		int color = colorpicker(vertex, G, colors);
 
 		// Set the color for that element
 		colors[vertex] = color;
 	}
 
-	deconstructor(queue);
+	listdeconstructor(V);
 
 	return colors;
+}
+
+
+int *miscoloring(graph *G) {
+	// List construction
+	list *V = emptylistconstructor();
+	for(uint i = 0; i < G->size; ++i) {
+		push(V, i);
+	}
+
+	// Resulting color array
+	int *colors = (int *) malloc(G->size * sizeof(int));
+	for(uint i = 0; i < G->size; ++i) {
+		colors[i] = NO_COLOR;
+	}
+
+	// Until the pile of vertices to be colored has not been handled:
+	while(!isempty(V)) {
+		// Pick one vertex
+		node *v = pop(V);
+		uint vertex = v->value;
+
+		// Compute a maximally independent set
+		list *I = mis(G);
+
+		// Pick a color for the MIS
+		int color = miscolorpicker(I, G, colors);
+
+		// Color all the vertices in the MIS
+		for(node *i = I->head; i != NULL; i = i->next){
+			colors[i->value] = color;
+			removelement(V, i->value);
+		}
+
+		listdeconstructor(I);
+	}
+
+	listdeconstructor(V);
+
+	return colors;
+}
+
+
+int miscolorpicker(list *I, graph *G, int *colors) {
+	int maxcolor = -1;
+
+	for(node *i = I->head; i != NULL; i = i->next) {
+		for(uint j = 0; j < G->size; ++j) {
+			if(G->vertices[i->value][j] == 1 && colors[j] > maxcolor) {
+				maxcolor = colors[j];
+			}
+		}
+	}
+
+	return maxcolor + 1;
 }
 
 
@@ -46,4 +104,18 @@ uint colorpicker(uint vertex, graph *G, int *colors) {
 	}
 
 	return maxcolor + 1;
+}
+
+
+void coloringwriter(int *coloring, uint size) {
+	char path[] = "../../testing-files/coloring.txt";
+
+	FILE *fptr = fopen(path, "w");
+
+	for(uint i = 0; i < size; ++i) {
+		fprintf(fptr, "%d,", coloring[i]);
+	}
+	fprintf(fptr, "\n");
+
+	fclose(fptr);
 }
--- graphs/coloring/coloring.h
+++ graphs/coloring/coloring.h
@@ -8,6 +8,12 @@
 
 	int *naivecoloring(graph *G);
 
+	int *miscoloring(graph *G);
+
+	int miscolorpicker(list *I, graph *G, int *colors);
+
 	uint colorpicker(uint vertex, graph *G, int *colors);
+
+	void coloringwriter(int *coloring, uint size);
 
 #endif
